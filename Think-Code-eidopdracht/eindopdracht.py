8c5924550f4bb4aa31fb4e3f4792740dd936fd31
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2021-11-03 14:24:11 +0100

    eindopdracht think code verbetering

--- Think-Code-eidopdracht/eindopdracht.py
+++ Think-Code-eidopdracht/eindopdracht.py
@@ -1,36 +1,39 @@
 #ik ben van plan om een quiz te maken.
-print("hello, wat is jouw naam")
+#------------------------------------------------------------------- vraag de naam van gebruiker.
+print("hallo, wat is jouw naam")
 name = input ("type je naam ")
-#-------------------------------------------------------------------
+#------------------------------------------------------------------- vraag gebruiker of ze een quiz willen maken.
 print("Welkom "+ name )
 start = input("wil je een quiz maken " + name + " ") 
 
 if start == "ja":
     print("mooi zo laten we beginnen met de eerste vraag. ")
+
+    #------------------------------------------------------------------- eerste vraag en antwoord.
+    eerste_vraag = input("vind je de opleiding leuk? ")
+
+    if eerste_vraag == "ja":
+
+        print("dat is mooi om te horen!")
+    else:
+        print("dat is jammer. Je kan nog van opleiding veranderen.")
+    #------------------------------------------------------------------- tweede vraag en antwoord.
+    tweede_vraag = input("heb je al je werk af? ")
+
+    if tweede_vraag == "ja":
+        print("goed zo je bent ook bijna klaar met de quiz!")
+    else:
+        print("waarom ben je hier?! ga je werk maken!")
+    #------------------------------------------------------------------- derde vraag en antwoord.
+    laaste_vraag = input("ben je trots op je zelf? ")
+
+    if laaste_vraag == "ja":
+        print("ik ben ook trots op jouw")
+    else:
+        print("ik ben ook teleurgesteld in je")
+    #------------------------------------------------------------------- zeg gedach tegen de gebruiker.
+    print("eind quiz")
+
 else:
     print("oke doei")
-#------------------------------------------------------------------
-eerste_vraag = input("vind je de opleiding leuk? ")
-
-if eerste_vraag == "ja":
-
-    print("dat is mooi om te horen!")
-else:
-    print("dat is jammer. Je kan nog van opleiding veranderen.")
-#-----------------------------------------------------------------
-tweede_vraag = input("heb je al je werk af? ")
-
-if tweede_vraag == "ja":
-    print("goed zo je bent ook bijna klaar met de quiz!")
-else:
-    print("waarom ben je hier?! ga je werk maken!")
-#-----------------------------------------------------------------
-laaste_vraag = input("ben je trots op je zelf? ")
-
-if laaste_vraag == "ja":
-    print("ik ben ook trots op jouw")
-else:
-    print("ik ben ook teleurgesteld in je")
-
-print("eind quiz")
 
